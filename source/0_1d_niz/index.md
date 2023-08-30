c41e34a6f0fe6459d175bb49e35d385f4c0b59c8
Velimir Radlovački <velimir.radlovacki@gmail.com>
2023-08-30 11:18:50 +0200

    Povratak na MARKDOWN

---
+++ source/0_1d_niz/index.md
@@ -0,0 +1,15 @@
+# Једнодимензионални низови
+
+## Садржај
+
+```{toctree}
+:maxdepth: 1
+
+11_uvod
+12_pretraga
+13_uredjivanje
+14_invertovanje
+15_rotiranje
+16_sazimanje
+17_prosirivanje
+```
---
+++ source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Програмирање II за информатичке профиле у електротехничким школама'
+copyright = '2023, Фондација Петља'
+author = 'Петља'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [ "myst_parser",
+               "sphinx_design",
+               "sphinx_copybutton",
+                "petlja_sphinx_extensions.extensions.notes",
+                "petlja_sphinx_extensions.extensions.multiple_choice"  ,            
+                "petlja_sphinx_extensions.extensions.fill_in_the_blank"]
+
+myst_enable_extensions = [ "colon_fence",
+                           "dollarmath",
+                           "html_admonition" ]
+templates_path = ['_templates']
+exclude_patterns = []
+language = 'sr'
+import petlja_sphinx_extensions
+html_static_path = petlja_sphinx_extensions.extensions.static_dirs()
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_title = 'Програмирање II за информатичке профиле у електротехничким школама'
+html_theme = 'furo'
